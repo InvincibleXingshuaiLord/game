619e1638b345d39ef88c395c86f1df2ca01b8eec
K L <LK1654691995@outlook.com>
2026-04-18 22:06:41 +0800

    增加了第一版开始界面

--- game/源.cpp
+++ game/源.cpp
@@ -308,11 +308,13 @@ void DrawEntities();
 
 //对象定义区
 
-functionalshape()
+//用于开始界面的功能图形绘制
+void functionalshape();
 
 
 int main()
 {
+    DrawStartUI();
     //初始化游戏
     GameInit();
     while (g_isRun)
@@ -512,7 +514,7 @@ bool CheckButtonClick(Button& btn) {
 void DrawButton(Button& btn, const char* text) {
 
 }
-
+//用于开始界面的功能图形绘制
 void functionalshape(int rx, int ry, int rw, int rh, std::string s) {
     setfillcolor(0X000000);
     solidrectangle(rx, ry, rx + rw, ry + rh);
@@ -526,7 +528,7 @@ void DrawStartUI() {
     initgraph(1000, 700);
 
     IMAGE img;
-    loadimage(&img, "photo/kk1.jpg", 1100, 700);
+    loadimage(&img, "photo/kk1.jpg", 1100, 700);//开始界面壁纸
     putimage(0, 0, &img);
 
     int rx, ry[5], rh, rw, i;
@@ -537,6 +539,7 @@ void DrawStartUI() {
     for (i = 1; i < 5; i++) {
         ry[i] = ry[i - 1] + rh + 10;
     }
+    //按钮图形绘制
     functionalshape(rx, ry[0], rw, rh, "开始游戏");
     functionalshape(rx, ry[1], rw, rh, "玩法介绍");
     functionalshape(rx, ry[2], rw, rh, "团队介绍");
